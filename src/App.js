5321c545eecd363af0d8e9e93ec1ba3fa4df9691
Minh <nguyentrongminh1412@gmail.com>
2022-04-07 17:15:05 +0700

    Call displayTodo in appjs, show list item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-// import DisplayTodos from './components/DisplayTodos';
+import './css/main.css';
+import DisplayTodos from './components/DisplayTodos';
 import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
       <Todos />
-      {/* <DisplayTodos /> */}
+      <DisplayTodos />
     </div>
   );
 }
