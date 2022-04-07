4966db0369a3c06eb682352b1410adce035d92b8
Minh <nguyentrongminh1412@gmail.com>
2022-04-07 17:17:58 +0700

    update function add, motion framer style component todo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addTodos } from '../redux/reducer';
+import { GoPlus } from 'react-icons/go';
+import { motion } from 'framer-motion';
 
 const mapStateToProps = (state) => {
   return {
@@ -17,18 +19,18 @@ const mapDispatchToProps = (dispatch) => {
 const Todos = (props) => {
   const [todo, setTodo] = useState('');
 
-  // const add = () => {
-  //   if (todo === '') {
-  //     alert('Input is Empty');
-  //   } else {
-  //     props.addTodo({
-  //       id: Math.floor(Math.random() * 1000),
-  //       item: todo,
-  //       complete: false,
-  //     });
-  //     setTodo('');
-  //   }
-  // };
+  const add = () => {
+    if (todo === '') {
+      alert('Input is Empty');
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo('');
+    }
+  };
 
   const handleChange = (e) => {
     setTodo(e.target.value);
@@ -43,26 +45,25 @@ const Todos = (props) => {
         className="todo-input"
         value={todo}
       />
-      <button
-        className="add-btn"
-        onClick={() =>
-          props.addTodo({
-            id: Math.floor(Math.random() * 1000),
-            item: todo,
-            completed: false,
-          })
-        }
-      >
-        Add
-      </button>
+      {/* <button className="add-btn" onClick={() => add()}>
+        <GoPlus />
+      </button> */}
       <br />
-
-      <ul>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="add-btn"
+        onClick={() => add()}
+      >
+        <GoPlus />
+      </motion.button>
+      <br />
+      {/* <ul>
         {props.todos.length > 0 &&
           props.todos.map((item) => {
             return <li key={item.id}>{item.item}</li>;
           })}
-      </ul>
+      </ul> */}
     </div>
   );
 };
